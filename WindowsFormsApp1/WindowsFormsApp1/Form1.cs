a00d93acb25264c67612cc671cc8e65499296725
student <balazs.saly@stud.uni-corvinus.hu>
2021-10-22 10:55:09 +0200

    kalóz

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -19,6 +19,7 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
             CreatePortfolio();
         }
@@ -29,6 +30,20 @@ namespace WindowsFormsApp1
             Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
 
             dataGridView2.DataSource = Portfolio;
+        }
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
         }
     }
 }
