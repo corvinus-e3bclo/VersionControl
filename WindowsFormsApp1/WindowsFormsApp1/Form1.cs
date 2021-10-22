248a6bea241297fe7b22ba081eb28e44fa5bf906
student <balazs.saly@stud.uni-corvinus.hu>
2021-10-22 10:48:54 +0200

    ticks

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -12,9 +12,12 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
