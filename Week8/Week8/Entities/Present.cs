008d29a72247225ac0bdbac8105cfdde683943b9
student <balazs.saly@stud.uni-corvinus.hu>
2021-11-12 17:44:01 +0100

    present

---
+++ Week8/Week8/Entities/Present.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week8.Abstractions;
+
+namespace Week8.Entities
+{
+    class Present : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
